db93d3753fcc8ffd51e4bee9c2381e3e72f7762b
ryantheengineer <ryan.larson.wa@gmail.com>
2026-05-18 13:08:21 -0600

    Create neural_agents.py

---
+++ engine/seeds/neural_agents.py
@@ -0,0 +1,11 @@
+from ..seed import MendelSeed
+
+class NeuralAgentSeed(MendelSeed):
+
+    @property
+    def name(self):
+        return "Neural Agent"
+
+    def random_genome(self):
+        # 
+        pass
